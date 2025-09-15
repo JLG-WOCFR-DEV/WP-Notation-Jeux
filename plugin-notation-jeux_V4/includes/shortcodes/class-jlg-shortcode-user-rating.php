a0a50fdccc478d5c6cf70c4977072f31e6ff552e
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-15 22:41:01 +0200

    Hash user IP when determining rating vote

--- plugin-notation-jeux_V4/includes/shortcodes/class-jlg-shortcode-user-rating.php
+++ plugin-notation-jeux_V4/includes/shortcodes/class-jlg-shortcode-user-rating.php
@@ -19,15 +19,17 @@ class JLG_Shortcode_User_Rating {
 
         $post_id = get_the_ID();
         $ratings = get_post_meta($post_id, '_jlg_user_ratings', true);
-        $has_voted = (is_array($ratings) && isset($ratings[$_SERVER['REMOTE_ADDR']]));
-        
+        $user_ip = isset($_SERVER['REMOTE_ADDR']) ? filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP) : false;
+        $user_ip_hash = $user_ip ? wp_hash($user_ip) : '';
+        $has_voted = (is_array($ratings) && $user_ip_hash && isset($ratings[$user_ip_hash]));
+
         return JLG_Frontend::get_template_html('shortcode-user-rating', [
             'options' => $options,
             'post_id' => $post_id,
             'avg_rating' => get_post_meta($post_id, '_jlg_user_rating_avg', true),
             'count' => get_post_meta($post_id, '_jlg_user_rating_count', true),
             'has_voted' => $has_voted,
-            'user_vote' => $has_voted ? $ratings[$_SERVER['REMOTE_ADDR']] : 0
+            'user_vote' => $has_voted ? $ratings[$user_ip_hash] : 0
         ]);
     }
 }
