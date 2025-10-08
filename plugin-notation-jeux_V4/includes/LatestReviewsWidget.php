8614c91cba53f8897066436c9b5ed890b3dbf8d1
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-08 14:08:42 +0200

    Clarify latest reviews widget slicing logic

--- plugin-notation-jeux_V4/includes/LatestReviewsWidget.php
+++ plugin-notation-jeux_V4/includes/LatestReviewsWidget.php
@@ -148,7 +148,8 @@ class LatestReviewsWidget extends WP_Widget {
 
         $max_ids = $post_limit * 3;
         if ( $max_ids > 0 && count( $post_ids ) > $max_ids ) {
-            $post_ids = array_slice( $post_ids, -$max_ids );
+            $start_index = count( $post_ids ) - $max_ids;
+            $post_ids    = array_slice( $post_ids, $start_index, null, false );
         }
 
         if ( empty( $post_ids ) ) {
