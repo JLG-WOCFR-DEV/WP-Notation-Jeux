bb4d3a3c95104a0a69ca6bc48f8c3f3f40ffa780
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-08 15:44:43 +0200

    Ensure Latest Reviews widget keeps newest rated IDs

--- plugin-notation-jeux_V4/includes/LatestReviewsWidget.php
+++ plugin-notation-jeux_V4/includes/LatestReviewsWidget.php
@@ -148,7 +148,8 @@ class LatestReviewsWidget extends WP_Widget {
 
         $max_ids = $post_limit * 3;
         if ( $max_ids > 0 && count( $post_ids ) > $max_ids ) {
-            $post_ids = array_slice( $post_ids, -$max_ids, null, false );
+            // Conserve the most recent rated posts by trimming from the end of the list.
+            $post_ids = array_slice( $post_ids, -$max_ids, $max_ids, false );
         }
 
         if ( empty( $post_ids ) ) {
