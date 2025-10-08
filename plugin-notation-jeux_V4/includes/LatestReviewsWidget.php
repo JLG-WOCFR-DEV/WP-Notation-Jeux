06456872153be0bd732f889a1d5fdbceb1268107
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-08 15:41:16 +0200

    Fix Latest Reviews widget to keep newest rated posts

--- plugin-notation-jeux_V4/includes/LatestReviewsWidget.php
+++ plugin-notation-jeux_V4/includes/LatestReviewsWidget.php
@@ -148,8 +148,7 @@ class LatestReviewsWidget extends WP_Widget {
 
         $max_ids = $post_limit * 3;
         if ( $max_ids > 0 && count( $post_ids ) > $max_ids ) {
-            $start_index = count( $post_ids ) - $max_ids;
-            $post_ids    = array_slice( $post_ids, $start_index, null, false );
+            $post_ids = array_slice( $post_ids, -$max_ids, null, false );
         }
 
         if ( empty( $post_ids ) ) {
