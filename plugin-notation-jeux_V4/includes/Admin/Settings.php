a5203e5da6640179de0df337ad402b6af9a1b853
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-16 23:32:59 +0200

    Fix rating badge threshold clamping

--- plugin-notation-jeux_V4/includes/Admin/Settings.php
+++ plugin-notation-jeux_V4/includes/Admin/Settings.php
@@ -486,6 +486,8 @@ class Settings {
     }
 
     private function post_process_schedule_score_scale_migration( array $sanitized, array $input, array $defaults, array $current_options, array $definition = array() ) {
+        unset( $definition );
+
         $previous_max = isset( $current_options['score_max'] )
             ? Helpers::get_score_max( array( 'score_max' => $current_options['score_max'] ) )
             : ( $defaults['score_max'] ?? 10 );
@@ -514,34 +516,11 @@ class Settings {
             $raw_threshold = trim( $raw_threshold );
         }
 
-        if ( ! is_numeric( $raw_threshold ) ) {
-            $raw_threshold = is_numeric( $sanitized['rating_badge_threshold'] )
-                ? (float) $sanitized['rating_badge_threshold']
-                : (float) ( $defaults['rating_badge_threshold'] ?? 0 );
-        } else {
-            $raw_threshold = (float) $raw_threshold;
-        }
-
-        $raw_threshold = max( 0.0, (float) $raw_threshold );
-
-        $score_max_reference = $sanitized['score_max'] ?? ( $defaults['score_max'] ?? 10 );
+        $score_max_reference = $sanitized['score_max']
+            ?? ( $current_options['score_max'] ?? ( $defaults['score_max'] ?? 10 ) );
 
         if ( ! is_numeric( $score_max_reference ) ) {
             $score_max_reference = Helpers::get_score_max( array( 'score_max' => $score_max_reference ) );
-        }
-    }
-
-    private function maybe_schedule_score_scale_migration( array $sanitized, array $context ) {
-        $current_options = $context['current'] ?? array();
-        $defaults        = $context['defaults'] ?? Helpers::get_default_settings();
-
-        $old_reference = isset( $current_options['score_max'] )
-            ? array( 'score_max' => $current_options['score_max'] )
-            : array( 'score_max' => $defaults['score_max'] ?? 10 );
-        $new_reference = array( 'score_max' => $sanitized['score_max'] ?? ( $defaults['score_max'] ?? 10 ) );
-
-        if ( is_numeric( $score_max_reference ) ) {
-            $raw_threshold = min( $raw_threshold, (float) $score_max_reference );
         }
 
         $constraints = array(
